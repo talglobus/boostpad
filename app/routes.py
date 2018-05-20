e9e7532f163ae444d634fc178150b3fe8dd3a393
Tal Globus <talglobus@gmail.com>
2018-05-19 20:46:23 -0700

    Add branding, privacy policy and login

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, send_from_directory
 from app import app
 from app.utils import nocache
+from app.config import config
 
 
 @app.route('/')
@@ -9,3 +10,14 @@ from app.utils import nocache
 def index():
 	user = {'username': 'Tal'}
 	return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/start')
+@nocache
+def start():
+	user = {'username': 'Tal'}
+	return render_template('start.html', title='Start', user=user, config=config)
+
+@app.route('/static/<path:path>')
+def send_js(path):
+	return send_from_directory('static', path)
