15ded6571a75a5da7b287cb8da297da6d0230808
Tal Globus <talglobus@gmail.com>
2018-05-20 09:39:07 -0700

    Try to add logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask
 
 app = Flask(__name__)
 # app.debug = True	# TODO: Find out why this isn't doing anything
 
 from app import routes
+
+handler = RotatingFileHandler('boostedlogs.log', maxBytes=10000, backupCount=1)
+handler.setLevel(logging.INFO)
+app.logger.addHandler(handler)
